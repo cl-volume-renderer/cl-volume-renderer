318c6559a8841a4ac1eeb3ce3945e12f170d5abd
Marcel Hollerbach <mail@marcel-hollerbach.de>
2020-06-17 12:22:42 +0200

    first version of sdl

--- app/renderer.cpp
+++ app/renderer.cpp
@@ -4,11 +4,13 @@
 std::vector<unsigned char> output = std::vector<unsigned char>(2048*1024*4);
 std::vector<char> buf_init(8*2);
 std::vector<short> ref_init(8);
+std::vector<short> sdf_init(8);
 
 renderer::renderer()
 : render_func(ctx, "ray_marching.cl", "render"),
   frame(ctx, std::move(output), {2048, 1024,1}),
   reference_volume(ctx, std::move(ref_init), {2,2,2}),
+  sdf(ctx, std::move(sdf_init), {2, 2, 2}),
   buffer_volume(ctx, std::move(buf_init))
 {
 
@@ -19,13 +21,55 @@ renderer::~renderer()
 
 }
 
+static unsigned long
+evenness(const unsigned int g, const unsigned int l)
+{
+   unsigned int mod = g % l;
+   if (g % l == 0) return g;
+
+   return g + 8 - mod;
+}
+
 void renderer::image_set(volume_block *b)
 {
+  //resources for rendering
   std::vector<char> buffer(b->m_voxel_count_x * b->m_voxel_count_y * b->m_voxel_count_z*2, 0);
   buffer_volume = clw_vector<char>(ctx, std::move(buffer));
   buffer_volume.push();
   reference_volume = clw_image<const short>(ctx, std::move(b->m_voxels), {b->m_voxel_count_x, b->m_voxel_count_y, b->m_voxel_count_z});
   reference_volume.push();
+
+  //resources for the sdf caclulation
+  std::vector<short> sdf_volume_buffer(b->m_voxel_count_x * b->m_voxel_count_y * b->m_voxel_count_z, 0);
+  sdf = clw_image<short>(ctx, std::move(sdf_volume_buffer), {b->m_voxel_count_x, b->m_voxel_count_y, b->m_voxel_count_z});
+  sdf.push();
+
+  clw_image<short> sdf_volume_pong = clw_image<short>(ctx, std::move(sdf_volume_buffer), {b->m_voxel_count_x, b->m_voxel_count_y, b->m_voxel_count_z});
+  sdf_volume_pong.push();
+
+  //first fill the sdf image with -1 for *inside* a interesting area 1 outside a interesting area
+  auto sdf_generation_init = clw_function(ctx, "signed_distance_field.cl", "create_boolean_image");
+  sdf_generation_init.execute({evenness(b->m_voxel_count_x, 8), evenness(b->m_voxel_count_y,8), evenness(b->m_voxel_count_z, 8)}, {8,8,8}, reference_volume, sdf);
+
+  //now iterate
+  std::vector<int> counter(1,0);
+  clw_vector<int> atomic_add_buffer(ctx, std::move(counter));
+  auto sdf_generation_initialization = clw_function(ctx, "signed_distance_field.cl", "create_signed_distance_field");
+  printf("Starting to build SDF\n");
+  for (int i = 0; i < 1000; ++i) {
+    atomic_add_buffer[0] = 0;
+    atomic_add_buffer.push();
+    //FIXME this is only needed because swap does not like clw_image
+    if (i % 2 == 0)
+      sdf_generation_initialization.execute({evenness(b->m_voxel_count_x, 8), evenness(b->m_voxel_count_y,8), evenness(b->m_voxel_count_z, 8)}, {8,8,8}, sdf, sdf_volume_pong, atomic_add_buffer);
+    else
+      sdf_generation_initialization.execute({evenness(b->m_voxel_count_x, 8), evenness(b->m_voxel_count_y,8), evenness(b->m_voxel_count_z, 8)}, {8,8,8}, sdf_volume_pong, sdf, atomic_add_buffer);
+    atomic_add_buffer.pull();
+    if (atomic_add_buffer[0] == 0 && i % 2 == 1) { //FIXME the later part is only required because sdf is not always filled with the correct values
+      break;
+    }
+  }
+  printf("Finished building SDF\n");
 }
 
 void* renderer::render_frame(struct ui_state &state, bool &frame_changed)
--- app/renderer.h
+++ app/renderer.h
@@ -11,6 +11,7 @@ class renderer : public frame_emitter {
     clw_function render_func;
     clw_image<unsigned char,4> frame; //frame output in 2D
     clw_image<const short> reference_volume; //image data input in 3D
+    clw_image<short> sdf;
     clw_vector<char> buffer_volume; //image data input in 3D
   public:
     renderer();
