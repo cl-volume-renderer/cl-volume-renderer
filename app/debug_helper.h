91eec494c39dc2a7938de1a6c0752552aae34c8d
somefoo <50917034+somefoo@users.noreply.github.com>
2020-06-18 15:22:18 +0200

    Added a basic timer function to the render-call

---
+++ app/debug_helper.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <chrono>
+
+#if 1
+#define TIME_FUNCTION(function, comment) \
+  {\
+  auto t1 = std::chrono::high_resolution_clock::now();\
+  function;\
+  auto t2 = std::chrono::high_resolution_clock::now();\
+  auto int_ms = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);\
+  std::cout << "[DEBUG TIMER] " << comment << int_ms.count()/1000.0f << "s\n";\
+  }
+
+#define TIME_START() auto t1_x0 = std::chrono::high_resolution_clock::now();
+#define TIME_RESET()      t1_x0 = std::chrono::high_resolution_clock::now();
+#define TIME_PRINT(comment) \
+{\
+  auto t2_x0 = std::chrono::high_resolution_clock::now();\
+  auto int_ms_x0 = std::chrono::duration_cast<std::chrono::milliseconds>(t2_x0 - t1_x0);\
+  std::cout << "[CUSTOM TIMER] " << comment << int_ms_x0.count()/1000.0f << "s\n";\
+}
+
+#define TIME_PRINT_RESET(comment) \
+{\
+  auto t2_x0 = std::chrono::high_resolution_clock::now();\
+  auto int_ms_x0 = std::chrono::duration_cast<std::chrono::milliseconds>(t2_x0 - t1_x0);\
+  std::cout << "[CUSTOM TIMER] " << comment << int_ms_x0.count()/1000.0f << "s\n";\
+  t1_x0 = std::chrono::high_resolution_clock::now();\
+}
+#else
+#define TIME_FUNCTION(function, comment) function;
+#define TIME_START()
+#define TIME_RESET()
+#define TIME_PRINT(comment)
+#define TIME_PRINT_RESET(comment)
+#endif
--- app/renderer.cpp
+++ app/renderer.cpp
@@ -1,5 +1,6 @@
 #include "renderer.h"
 #include "nrrd_loader.h"
+#include "debug_helper.h"
 
 std::vector<unsigned char> output = std::vector<unsigned char>(2048*1024*4);
 std::vector<char> buf_init(8*2);
@@ -49,7 +50,7 @@ void renderer::image_set(volume_block *b)
 
   //first fill the sdf image with -1 for *inside* a interesting area 1 outside a interesting area
   auto sdf_generation_init = clw_function(ctx, "signed_distance_field.cl", "create_boolean_image");
-  sdf_generation_init.execute({evenness(b->m_voxel_count_x, 8), evenness(b->m_voxel_count_y,8), evenness(b->m_voxel_count_z, 8)}, {8,8,8}, reference_volume, sdf);
+  sdf_generation_init.execute({evenness(b->m_voxel_count_x, 8), evenness(b->m_voxel_count_y,8), evenness(b->m_voxel_count_z, 8)}, {4,4,4}, reference_volume, sdf);
 
   //now iterate
   std::vector<int> counter(1,0);
@@ -61,9 +62,9 @@ void renderer::image_set(volume_block *b)
     atomic_add_buffer.push();
     //FIXME this is only needed because swap does not like clw_image
     if (i % 2 == 0)
-      sdf_generation_initialization.execute({evenness(b->m_voxel_count_x, 8), evenness(b->m_voxel_count_y,8), evenness(b->m_voxel_count_z, 8)}, {8,8,8}, sdf, sdf_volume_pong, atomic_add_buffer);
+      sdf_generation_initialization.execute({evenness(b->m_voxel_count_x, 8), evenness(b->m_voxel_count_y,8), evenness(b->m_voxel_count_z, 8)}, {4,4,4}, sdf, sdf_volume_pong, atomic_add_buffer);
     else
-      sdf_generation_initialization.execute({evenness(b->m_voxel_count_x, 8), evenness(b->m_voxel_count_y,8), evenness(b->m_voxel_count_z, 8)}, {8,8,8}, sdf_volume_pong, sdf, atomic_add_buffer);
+      sdf_generation_initialization.execute({evenness(b->m_voxel_count_x, 8), evenness(b->m_voxel_count_y,8), evenness(b->m_voxel_count_z, 8)}, {4,4,4}, sdf_volume_pong, sdf, atomic_add_buffer);
     atomic_add_buffer.pull();
     if (atomic_add_buffer[0] == 0 && i % 2 == 1) { //FIXME the later part is only required because sdf is not always filled with the correct values
       break;
@@ -92,11 +93,14 @@ void* renderer::render_frame(struct ui_state &state, bool &frame_changed)
   direction_vector[1] /= length;
   direction_vector[2] /= length;
 
+  TIME_START();
   render_func.execute({(unsigned long)state.width, (unsigned long)state.height}, {8, 8}, frame,
     reference_volume, buffer_volume,
     state.position.val[0], state.position.val[1], state.position.val[2],
     direction_vector[0], direction_vector[1], direction_vector[2]);
+
   frame.pull();
+  TIME_PRINT("Render time");
 
   state.cam_changed = false;
   state.path_changed = false;
