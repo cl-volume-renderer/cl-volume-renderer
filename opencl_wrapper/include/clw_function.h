681b140283369765dab017ea92082976a666d4b1
somefoo <50917034+somefoo@users.noreply.github.com>
2020-06-20 21:51:22 +0200

    Allow the including in opencl cl code         Use #clw_include "example.h"

--- opencl_wrapper/include/clw_function.h
+++ opencl_wrapper/include/clw_function.h
@@ -8,6 +8,8 @@
 #include <type_traits>
 #include <vector>
 #include <tuple>
+#include <filesystem>
+#include <algorithm>
 #include "clw_context.h"
 #include "clw_helper.h"
 #include "clw_vector.h"
@@ -55,8 +57,87 @@ class clw_function{
     m_kernel = clCreateKernel(m_program, function_name.data(), NULL);
     clw_fail_hard_on_error(error);
   }
-  clw_function(const clw_context& context, const std::string& path, const std::string& function_name)
-    : clw_function(context, (const std::vector<std::string>){path}, function_name){
+
+  const std::string do_include(const std::string& path, std::vector<std::string>& already_included){
+    std::filesystem::path path_to_file(path);
+    std::filesystem::path directory_of_file = path_to_file.parent_path();
+    std::ifstream file_stream(path_to_file);
+
+    std::string code_string = "";
+  
+    //Ensure each file is only included once!
+    if(std::find(already_included.begin(), already_included.end(), path_to_file.lexically_normal()) != already_included.end()){
+      file_stream.close();
+      return "";
+    }
+
+    if(file_stream.fail()){
+      std::cerr << "Failed to open file: " << path_to_file << "\n";
+      exit(1);
+    }
+
+    already_included.push_back(path_to_file.lexically_normal());
+    
+    
+    std::string line;
+    while(std::getline(file_stream, line)){
+      size_t include_position = line.find("#clw_include");
+      if(include_position != std::string::npos){
+        size_t include_start = line.find_first_of('"', include_position);
+        size_t include_end = line.find_first_of('"', include_start + 1);
+        if(include_start == std::string::npos || include_end == std::string::npos){
+          std::cout << "Error when parsing OpenCL program: \n"
+                    << " #cl_include not of the expected format: cl_include \"...\"\n";
+          exit(1);
+        }
+        include_start++;
+        size_t include_size = include_end - include_start;
+        size_t include_size_all = include_end - include_position + 1;
+        
+        std::string include_file_name = line.substr(include_start, include_size);
+
+        std::filesystem::path tmp_dir = directory_of_file;
+        std::string include_content = do_include(directory_of_file/include_file_name, already_included);
+        line.replace(include_position, include_size_all, include_content);
+        code_string += line + '\n';
+      }else{
+        code_string += line + '\n'; 
+      }
+    }
+    file_stream.close();
+
+    return code_string;
+  }
+
+  clw_function(const clw_context& context, const std::string& path, const std::string& function_name): m_function_name(function_name), m_context(context){
+    std::vector<std::string> already_included;
+    //std::cout << do_include(/*KERNEL_DIR +*/ path, already_included) << '\n';
+		std::string opencl_code = do_include(KERNEL_DIR + path, already_included);
+		
+    cl_int error;
+    const char* indirection[1]{opencl_code.data()};
+    m_program = clCreateProgramWithSource(m_context.get_cl_context(), 1, indirection, NULL, &error);
+    clw_fail_hard_on_error(error);
+
+    error = clBuildProgram(m_program, 0, NULL, "-cl-mad-enable -cl-std=CL2.0", NULL, NULL);
+    if(error != CL_SUCCESS){
+      //Failed, print the error log
+      std::cout << "Kernel failed to compile:\n";
+      std::array<char, 4096> buffer;
+      error = clGetProgramBuildInfo(m_program, m_context.get_cl_device_id(), CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer.data(), NULL);
+      std::cout << "---------------------------------------\n";
+      std::cout << "\033[1;33m" << buffer.data() << "\033[0m" << '\n';
+      std::cout << "---------------------------------------\n";
+      clw_fail_hard_on_error(error);
+    }
+    clw_fail_hard_on_error(error);
+
+    cl_build_status build_status;
+    error = clGetProgramBuildInfo(m_program, m_context.get_cl_device_id(), CL_PROGRAM_BUILD_STATUS, sizeof(cl_build_status), &build_status, NULL);
+
+    m_kernel = clCreateKernel(m_program, function_name.data(), NULL);
+    clw_fail_hard_on_error(error); 
+
   }
 
   ~clw_function(){
