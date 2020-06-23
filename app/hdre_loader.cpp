b1367388f42669de64eeb9a11f647db2cb5eaa20
somefoo <50917034+somefoo@users.noreply.github.com>
2020-06-23 11:43:08 +0200

    Fixed the meson config missing cpp for image

--- app/hdre_loader.cpp
+++ app/hdre_loader.cpp
@@ -8,14 +8,16 @@ image hdre_loader::load_file(std::string path) {
   int width, height, n;
   stbi_info(path.data(), &width, &height, &n);
 
-  unsigned char *data = stbi_load(path.data(), &width, &height, &n, 0);
+  unsigned char *data = stbi_load(path.data(), &width, &height, &n, 4);
+  n = 4; //Problem with stb?
   if(data == NULL){
     std::cerr << "Error, failed to load file: " + path << "\n";
     exit(1);
   
   }
   std::vector<unsigned char> image_data(data, data + width*height*n);
-  delete[] data;
+
+  stbi_image_free(data);
   return image(std::move(image_data), width, height, n);
 }
 
