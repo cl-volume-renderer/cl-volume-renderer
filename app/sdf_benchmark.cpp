88023347e8999613323e2e32ffcbfc7b8d6178e2
Marcel Hollerbach <mail@marcel-hollerbach.de>
2020-07-01 16:44:47 +0200

    here comes a sdf benchmark
    that is actaully old, just cleaning up the local tree

---
+++ app/sdf_benchmark.cpp
@@ -0,0 +1,22 @@
+#include <signed_distance_field.h>
+#include <nrrd_loader.h>
+
+int main(int argc, char const *argv[])
+{
+  clw_context ctx;
+
+  nrrd_loader loader;
+
+  volume_block b = loader.load_file(argv[1]);
+
+  reference_volume rv(ctx, &b);
+
+  for (int i = 0; i < 100; ++i)
+    {
+       TIME_START();
+       signed_distance_field(ctx, rv, "inline bool is_event_gen(short value, short gradient){ return (value > 800); }");
+       TIME_PRINT("Loop Iteration");
+    }
+
+  return 0;
+}
